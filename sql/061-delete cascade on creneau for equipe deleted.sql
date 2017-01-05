25008f1cc5ab831a3e5415d8a6d11faff87eb029
Benjamin ALLEMAND <benallemand@gmail.com>
2017-01-05 20:27:06 +0100

    delete cascade on creneau for equipe deleted

--- sql/061-delete cascade on creneau for equipe deleted.sql
+++ sql/061-delete cascade on creneau for equipe deleted.sql
@@ -6,4 +6,14 @@ ALTER TABLE creneau
 ALTER TABLE creneau
   ADD CONSTRAINT fk_creneau_equipes FOREIGN KEY (id_equipe) REFERENCES equipes (id_equipe)
   ON DELETE CASCADE;
+
+ALTER TABLE joueur_equipe
+  DROP FOREIGN KEY fk_joueur_equipe_equipe;
+ALTER TABLE joueur_equipe
+  DROP INDEX fk_joueur_equipe_equipe;
+ALTER TABLE joueur_equipe
+  ADD CONSTRAINT fk_joueur_equipe_equipe FOREIGN KEY (id_equipe) REFERENCES equipes (id_equipe)
+  ON DELETE CASCADE;
+
+
 SET FOREIGN_KEY_CHECKS=1;
