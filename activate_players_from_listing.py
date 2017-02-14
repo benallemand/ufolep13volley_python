db924171b721ef5f4934520ee08417a7054684dc
Benjamin ALLEMAND <benallemand@gmail.com>
2017-02-14 18:34:31 +0100

    ufolep13volley-31 take into account last listing of players

--- activate_players_from_listing.py
+++ activate_players_from_listing.py
@@ -6,4 +6,5 @@ import sql_manager
 licences = data_manager.get_listing_volley_licences()
 if len(licences) <= 0:
     exit()
+print "Found %s licences" % len(licences)
 sql_manager.activate_players(licences)
