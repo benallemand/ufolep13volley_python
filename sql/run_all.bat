caab88e62372ed62f38b829a9e9168a604fec9ac
Benjamin ALLEMAND <benallemand@gmail.com>
2018-10-23 21:06:12 +0200

    ignorer les fichiers de backup et les fichiers de projet pycharm. ajout d'un script de rollback sur un backup

---
+++ sql/run_all.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+ECHO Running script
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" --default-character-set=utf8 -h localhost -u root ufolep_13volley < ufolepvocbufolep.sql
+
+ECHO Finished!
+pause
