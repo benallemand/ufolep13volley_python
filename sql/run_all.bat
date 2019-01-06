96017b31be9ebff4f4dcc745bafb53ccbbe01cb7
Benjamin ALLEMAND <benallemand@gmail.com>
2019-01-06 14:20:29 +0100

    ufolep13volley-108 créer un champ pour prioriser les créneaux

--- sql/run_all.bat
+++ sql/run_all.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 ECHO Running script
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" --default-character-set=utf8 -h localhost -u root ufolep_13volley < support\\_01-recreate_database.sql
 "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" --default-character-set=utf8 -h localhost -u root ufolep_13volley < ufolepvocbufolep.sql
 
 ECHO Finished!
---
+++ sql/support/_01-recreate_database.sql
@@ -0,0 +1,2 @@
+DROP DATABASE ufolep_13volley;
+CREATE DATABASE ufolep_13volley;
