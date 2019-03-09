2c71955733649a528024076e2a1edc0a4025b270
Benjamin ALLEMAND <benallemand@gmail.com>
2019-03-09 11:35:11 +0100

    déplacement de fichiers dans dossier updates

--- sql/run_all.bat
+++ sql/run_all.bat
@@ -5,4 +5,3 @@ ECHO Running script
 "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" --default-character-set=utf8 -h localhost -u root ufolep_13volley < ufolepvocbufolep.sql
 
 ECHO Finished!
-pause
