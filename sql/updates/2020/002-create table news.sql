42ddaea3666171b92b2fd4624bef04448c7679a3
Benjamin ALLEMAND <benallemand@gmail.com>
2020-04-27 21:43:19 +0200

    ajout d'une table news pour affichage sur la page d'accueil

---
+++ sql/updates/2020/002-create table news.sql
@@ -0,0 +1,5 @@
+CREATE TABLE news (
+    id bigint(10) PRIMARY KEY AUTO_INCREMENT,
+    title varchar(500) NOT NULL,
+    file_path varchar(200) NOT NULL
+);
---
+++ sql/updates/2020/003-insert news.sql
@@ -0,0 +1,3 @@
+INSERT INTO news
+SET title     = 'Arrêt de la saison 2019-2020',
+    file_path = '/news_files/20200427.pdf';
