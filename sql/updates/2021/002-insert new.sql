cf8b931ed9c43c248c4c3fa0cc4e32ff806790e9
Benjamin ALLEMAND <benallemand@gmail.com>
2021-10-06 15:31:51 +0200

    news-display-text

---
+++ sql/updates/2021/002-insert new.sql
@@ -0,0 +1,8 @@
+INSERT INTO news
+SET title = 'Important ! Aux responsables de club',
+    text="<h1>Aux responsables de clubs</h1><br/>
+            Si vous ne l'avez pas déjà fait suite au message envoyé par le Responsable de la CTSD,<br/>
+            envoyez le nombre d'équipes que vous pensez engager pour la saison 2021-22 en précisant le championnat dans lequel elles évolueraient,<br/>
+            à l'adresse <a href='mailto:laurent.gorlier@ufolep13volley.org'>laurent.gorlier@ufolep13volley.org</a>.<br/>
+            Merci pour vos retours et merci à ceux qui ont déjà répondu.<br/>
+            La CTSD";
