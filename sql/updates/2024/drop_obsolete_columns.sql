2961be6523a6341c1d6451c2f6c1ec881ee1b8af
Benjamin ALLEMAND <benallemand@gmail.com>
2024-12-04 22:24:41 +0100

    drop obsolete columns

--- sql/updates/2024/drop_obsolete_columns.sql
+++ sql/updates/2024/drop_obsolete_columns.sql
@@ -1,1 +1,6 @@
+-- DEV: DONE 241204
+-- PROD: DONE 241204
 ALTER TABLE joueurs DROP COLUMN est_actif;
+ALTER TABLE matches DROP COLUMN forfait_dom;
+ALTER TABLE matches DROP COLUMN forfait_ext;
+ALTER TABLE matches DROP COLUMN sheet_received;
