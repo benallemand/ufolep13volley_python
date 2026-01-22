c61983558c4fa4638d02a192f56c4463e4a8d4cf
Benjamin ALLEMAND <benallemand@gmail.com>
2026-01-22 22:20:50 +0100

    ajout de la date de déploiement #175

--- sql/updates/2026/001-add_remarques_to_gymnase.sql
+++ sql/updates/2026/001-add_remarques_to_gymnase.sql
@@ -1,6 +1,9 @@
 -- Issue #175: Ajouter un champ "remarques" pour les gymnases
 -- Permet de stocker des informations pratiques (ex: code portillon)
 
+-- DEV: DONE 260122
+-- PROD: DONE 260122
+
 ALTER TABLE gymnase 
 ADD COLUMN remarques TEXT NULL 
 AFTER nb_terrain;
